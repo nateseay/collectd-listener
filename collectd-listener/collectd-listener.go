97386ec54a24ffc11e6ad283be260df6afd6810f
Nate Seay <nate.seay@gmail.com>
2016-05-24 16:59:13 -0700

    Add Time/Interval Decoding
    - Needed to add code to read the timezone, so we can have local and UTC
      times
    - Handles both HighRes and non-HighRes times
    - Some of the framework for Values, but will need to figure out how to
      read the unique Values structure

--- collectd-listener/collectd-listener.go
+++ collectd-listener/collectd-listener.go
@@ -7,12 +7,15 @@ import (
   "flag"
   "github.com/nateseay/collectd-listener/parse"
   "github.com/nateseay/collectd-listener/logging"
+  "bufio"
+  "time"
 )
 
 type args_struct struct {
   udp_port *int
   typesdb_path *string
   log_level *int
+  timezone_file *string
 }
 
 func parse_args() args_struct{
@@ -20,6 +23,7 @@ func parse_args() args_struct{
   args.udp_port = flag.Int("udp-port", 8096, "UDP port to listen for collectd data" )
   args.typesdb_path = flag.String("typesdb-path", "./typedb", "Path to the collectd typesdb file")
   args.log_level = flag.Int("log-level", 2, "Log level, 1-4. 1 is least verbose, 4 is most")
+  args.timezone_file = flag.String("timezone", "/etc/timezone", "Timezone file location. Will default to America/LosAngeles if it can't be loaded")
   flag.Parse()
   return args
 }
@@ -31,8 +35,31 @@ func check_error(logger logging.LoggerStruct, err error){
   }
 }
 
+func loadTimezone(logger logging.LoggerStruct,
+                  args args_struct) time.Location{
+  logger.Trace.Println(fmt.Sprintf("Trying to load timezone file from %v", *args.timezone_file))
+  DefTZ := "America/LosAngeles"
+  file, err := os.Open(*args.timezone_file)
+  if err != nil {
+    logger.Error.Println(fmt.Sprintf("Unable to open timezone file. Falling back to %v. Err=%v", DefTZ, err))
+    DefaultLoc, _ := time.LoadLocation(DefTZ)
+    return *DefaultLoc
+  }
+  defer file.Close()
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan(){
+    lines = append(lines, scanner.Text())
+  }
+  SetLoc, _ := time.LoadLocation(lines[0])
+  logger.Info.Println(fmt.Sprintf("Timezone set to %v", SetLoc))
+  return *SetLoc
+}
+
 func run_server(logger logging.LoggerStruct,
                 args args_struct) {
+  /* Load timezone info */
+  TimeZoneLoc := loadTimezone(logger, args)
   /* Lets prepare a address at any address at port 10001*/
   logger.Info.Println(fmt.Sprintf("Preparing to bind to %v", *args.udp_port))
   ServerAddr,err := net.ResolveUDPAddr("udp",fmt.Sprintf(":%v", *args.udp_port))
@@ -53,7 +80,7 @@ func run_server(logger logging.LoggerStruct,
       if err != nil {
           logger.Error.Println("Error: ",err)
       }
-      parse.ParseBuffer(logger, buf)
+      parse.ParseBuffer(logger, buf, TimeZoneLoc)
   }
 
 }
@@ -64,5 +91,6 @@ func main(){
   logger.Info.Println("[Config] udp-port:", *args.udp_port)
   logger.Info.Println("[Config] typedb_path:", *args.typesdb_path)
   logger.Info.Println("[Config] log_level:", *args.log_level)
+  logger.Info.Println("[Config] timezone_file:", *args.timezone_file)
   run_server(logger, args)
 }
--- parse/parse.go
+++ parse/parse.go
@@ -9,6 +9,7 @@ import (
   "encoding/binary"
   "fmt"
   "github.com/nateseay/collectd-listener/logging"
+  "time"
 )
 
 
@@ -32,26 +33,73 @@ const (
   Encryption PartType = 0x0210 // TODO in the spec
 )
 
+type DataType uint8
+const (
+  COUNTER DataType = 0
+  GAUGE DataType = 1
+  DERIVE DataType = 2
+  ABSOLUTE DataType = 3
+)
+
+//http://stackoverflow.com/questions/28012952/golang-variable-length-array-in-struct-for-use-with-binary-read
 type BinaryProtocolHeader struct {
   BinaryType PartType
   DataLength uint16
 
 }
 
+type BinaryValuesHeade struct {
+
+
+}
+
+func timeDecode( logger logging.LoggerStruct,
+                  IsHighRes bool,
+                  value uint64,
+                  TimeZoneLoc time.Location) string {
+  var secs int64
+  var usecs uint64
+  secs = 0
+  usecs = 0
+  if IsHighRes{
+    usecs = value >> 30
+  } else{
+    usecs = value
+  }
+  secs = int64(usecs)
+  utc := time.Unix(secs, 0)
+  local := utc.In(&TimeZoneLoc)
+  ret := fmt.Sprintf("Epoch: %v Timestamp UTC: %v Timestamp Local: %v", secs, utc, local)
+  return ret
+}
+
 func parsePart( logger logging.LoggerStruct,
                 part []byte,
-                header BinaryProtocolHeader){
+                header BinaryProtocolHeader,
+                TimeZoneLoc time.Location){
   switch header.BinaryType {
+    // These are the string types
     case Host, Plugin, PluginInstance, CPartType, CPartTypeInstance, Message:
       PartString := string(part)
       logger.Info.Println(fmt.Sprintf("Decoded Part: type=%v value=%v", header.BinaryType, PartString))
+    // These are the number seconds since epoch values
+  case Time, Interval, TimeHighRes, IntervalHighRes:
+      Epoch := binary.BigEndian.Uint64(part)
+      var HighRes bool
+      HighRes = false
+      if (header.BinaryType == TimeHighRes) || (header.BinaryType == IntervalHighRes){
+        HighRes = true
+      }
+      TimeString := timeDecode(logger, HighRes, Epoch, TimeZoneLoc)
+      logger.Info.Println(fmt.Sprintf("Decoded Part: type=%v raw_value=%v value/s=%v", TimeString, Epoch, TimeString ))
     default:
       logger.Info.Println(fmt.Sprintf("Decoded Part: type %v not implemented yet!", header.BinaryType))
   }
  }
 
 func ParseBuffer ( logger logging.LoggerStruct,
-                    buf []byte ) {
+                    buf []byte ,
+                    TimeZoneLoc time.Location) {
   logger.Trace.Println("In ParseBuffer")
   // create a reader
   reader := bytes.NewReader(buf)
@@ -64,6 +112,6 @@ func ParseBuffer ( logger logging.LoggerStruct,
     part := make([]byte, (header.DataLength-4)) // -4 is to account for the header which has been read, but is included in DataLength
     err2 := binary.Read(reader, binary.BigEndian, part)
     logger.Trace.Println(fmt.Sprintf("Read Part err=%v buffer length remaining=%v", err2, reader.Len()))
-    parsePart(logger, part, header)
+    parsePart(logger, part, header, TimeZoneLoc)
   }
 }
